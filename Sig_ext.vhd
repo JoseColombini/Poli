b661bd2a50e0b01e640fffddf5fa77a33bbf7cfb
JoseColombini <gorinorr@gmail.com>
2020-05-02 18:51:33 -0300

    aaaa

--- Sig_ext.vhd
+++ Sig_ext.vhd
@@ -12,8 +12,10 @@ architecture extensor of signExtend is
 
   signal D  : bit_vector(1 downto 0);
   signal CB : bit_vector(1 downto 0);
+
   signal B  : bit;
 
+  signal HI : bit;
 begin
 
   D <= "11";
@@ -24,5 +26,7 @@ begin
  o <= (63 downto 9 => i(20)) & i(20 downto 12) when i(31 downto 30) = D else
       (63 downto 19 => i(23)) & i(23 downto 5) when i(31 downto 30) = CB else
       (63 downto 26 => i(25)) & i(25 downto 0) when i(31) = B;
+
+H <= '1';
 
 end architecture;
